7ce2e05a00f6109977ec8ffdc65a15bf867cffd0
farhan-ansari-1 <ansarifarhan172006@gmail.com>
2025-08-15 03:37:57 +0530

    Refined website visuals and responsiveness

--- script.js
+++ script.js
@@ -55,3 +55,20 @@ tabBtns.forEach((btn) => {
     }
   });
 });
+
+// Hamburger Menu Logic
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+navLinks.forEach((n) => n.addEventListener("click", closeMenu));
